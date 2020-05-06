58832106404aa2eafb7fe556e93b93805ae42b5b
FLeonardoS <00174620@uca.edu.sv>
2020-05-06 11:52:58 -0600

    primer programa

---
+++ laboratorio1.cpp
@@ -0,0 +1,12 @@
+#include "iostream"
+using namespace std;
+
+int main ()
+{
+    int numero;
+    cout <<"Ingresa un numero please" <<endl;
+    cin >> numero;
+    cout <<"El numero que eligio es: " <<endl;
+    cout << numero;
+    
+}
